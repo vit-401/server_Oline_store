9419e8689766016d45316372851ff327af8f1277
@V.Prisyagnuk <v.prisyagnuk@esverito.com>
2021-11-29 16:24:15 +0200

    added clienApp

--- routes/testRouter.js
+++
@@ -1,9 +0,0 @@
-import {Router} from "express"
-import testController from "../controllers/testController.js"
-
-const router = new Router()
-
-router.post('/',  testController.create)
-router.get('/', testController.getAll)
-
-export default router
